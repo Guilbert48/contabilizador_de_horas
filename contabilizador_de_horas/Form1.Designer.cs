5ae694d960d0a8a2a292d4e3f622722e503d99a3
guilbert silva <guilberts48@gmail.com>
2024-01-13 18:24:53 -0300

    Adicionar arquivos de projeto.

---
+++ contabilizador_de_horas/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace contabilizador_de_horas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.SeaGreen;
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(109, 270);
+            button1.Name = "button1";
+            button1.Size = new Size(275, 109);
+            button1.TabIndex = 0;
+            button1.Text = "Iniciar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkOrange;
+            button2.ForeColor = SystemColors.Control;
+            button2.Location = new Point(109, 402);
+            button2.Name = "button2";
+            button2.Size = new Size(275, 107);
+            button2.TabIndex = 1;
+            button2.Text = "Pausar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Stencil", 80.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(81, 105);
+            label1.Name = "label1";
+            label1.Size = new Size(333, 127);
+            label1.TabIndex = 2;
+            label1.Text = "00:00";
+            label1.Click += label1_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.DarkRed;
+            button3.ForeColor = SystemColors.Control;
+            button3.Location = new Point(109, 532);
+            button3.Name = "button3";
+            button3.Size = new Size(275, 107);
+            button3.TabIndex = 3;
+            button3.Text = "Parar";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(448, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(35, 29);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LemonChiffon;
+            ClientSize = new Size(486, 690);
+            Controls.Add(pictureBox1);
+            Controls.Add(button3);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Button button3;
+        private PictureBox pictureBox1;
+    }
+}
