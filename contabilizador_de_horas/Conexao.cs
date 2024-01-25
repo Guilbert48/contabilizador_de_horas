e8e5245c39cc5600d23f5232ac4223cb61013dd6
guilbert silva <guilberts48@gmail.com>
2024-01-25 11:14:52 -0300

    Criação da segunda camada: Camada de conexão

---
+++ contabilizador_de_horas/Conexao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace contabilizador_de_horas
+{
+    public class Conexao
+    {
+        SqlConnection con = new SqlConnection();
+        public Conexao() 
+        {
+            con.ConnectionString = "Data Source=DESKTOP-RVMH717;Initial Catalog=Contador_dba;Integrated Security=True;Encrypt=False"; 
+        }
+        public SqlConnection Conectar()
+        {
+            return con;
+        }
+
+    }
+}
---
+++ contabilizador_de_horas/SqlConnection.cs
@@ -0,0 +1,7 @@
+﻿namespace contabilizador_de_horas
+{
+    public class SqlConnection
+    {
+        public string ConnectionString { get; internal set; }
+    }
+}
